e54d0e84cbb7a4f8c1c40411e3d5e31dfaccd628
Milena Tomova <kokolina1888@gmail.com>
2017-04-27 22:46:46 +0300

    Up to master.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Home page</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+@extends('layout.master')
 
-<script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
+@section('title', 'Home')
+@section('content')
 <div class="container">
     <div class="content">
         <div class="title">Home Page</div>
         <div class="quote">Our Home page!</div>
     </div>
 </div>
-</body>
-</html>
-</head>
-<body>
-	
-</body>
-</html>
+@stop
+
+
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+<script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+@include('shared.navbar')
+
+@yield('content')	
+</body>
+</html>
